21633ff3f9753ed9a15e6bcad041a542107c0a65
VijayKagupati <vijaykumarkagupati@gmail.com>
2025-06-04 06:10:10 +0530

    v0.0.2.1
    Phase 1 Competition

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -7,7 +7,6 @@ public class MainMenuController : MonoBehaviour
 
     [Header("UI Elements")]
     public TextMeshProUGUI streakText;
-    public TextMeshProUGUI welcomeText;
     
     private void Start()
     {
@@ -19,19 +18,7 @@ public class MainMenuController : MonoBehaviour
         if (streakText)
         {
             int streak = DataManager.Instance.playerData.currentStreak;
-            streakText.text = streak > 0 ? $"You're on a {streak}-day streak!" : "Start your streak today!";
-        }
-        
-        if (welcomeText)
-        {
-            if (DataManager.Instance.playerData.lastPlayedDate == System.DateTime.MinValue)
-            {
-                welcomeText.text = "Welcome to BookWorm Learning!";
-            }
-            else
-            {
-                welcomeText.text = "Welcome back to BookWorm Learning!";
-            }
+            streakText.text = streak > 0 ? $"Your Current Streak is {streak} Day(s)" : "Start your streak today!";
         }
     }
 }
